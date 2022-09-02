3ae311caf759701397f3bdc8b2e392ab9e475824
paynechu <payne@piplay.com>
2022-09-02 16:37:58 +0800

    Added ImageSet's node to compare with SpriteAtlas's node
    - ImageSet's node have no problem work under ODR and TestFlight but SpriteAtla's node can't

--- TestODRWithSpriteAtlas Shared/GameScene.swift
+++ TestODRWithSpriteAtlas Shared/GameScene.swift
@@ -29,17 +29,31 @@ class GameScene: SKScene {
     }
     
     func setUpNodes() {
+        // PDF SpriteAtlas
         let atlasA = SKTextureAtlas(named: "Sprites-A")
         let texA = atlasA.textureNamed("PdfSprite")
-        let pdfNode = SKSpriteNode(texture: texA, size: texA.size())
-        pdfNode.position = CGPoint(x: 100.0, y: 100.0)
-        self.addChild(pdfNode)
+        let nodeA = SKSpriteNode(texture: texA, size: texA.size())
+        nodeA.position = CGPoint(x: 100.0, y: 100.0)
+        self.addChild(nodeA)
         
+        // PNG SpriteAtlas
         let atlasB = SKTextureAtlas(named: "Sprites-B")
         let texB = atlasB.textureNamed("PngSprite")
-        let pngNode = SKSpriteNode(texture: texB, size: texB.size())
-        pngNode.position = CGPoint(x: -100.0, y: -100.0)
-        self.addChild(pngNode)
+        let nodeB = SKSpriteNode(texture: texB, size: texB.size())
+        nodeB.position = CGPoint(x: -100.0, y: -100.0)
+        self.addChild(nodeB)
+        
+        // PDF ImageSet
+        let texC = SKTexture(imageNamed: "Pdf")
+        let nodeC = SKSpriteNode(texture: texC, size: texC.size())
+        nodeC.position = CGPoint(x: 100.0, y: -100.0)
+        self.addChild(nodeC)
+        
+        // PNG ImageSet
+        let texD = SKTexture(imageNamed: "Png")
+        let nodeD = SKSpriteNode(texture: texD, size: texD.size())
+        nodeD.position = CGPoint(x: -100.0, y: 100.0)
+        self.addChild(nodeD)
     }
     
     #if os(watchOS)
